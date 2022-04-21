5ab790e5f33018cf25c56c615ac9919fa2424e50
Leah Weber <leahlw2@ada.cs.illinois.edu>
2022-04-21 11:14:23 -0500

    added documenation

--- phertilizer/clonal_tree_list.py
+++ phertilizer/clonal_tree_list.py
@@ -75,43 +75,142 @@ class ClonalTreeList:
         self.stack = deque()
 
     def insert(self, tree):
+        '''inserts a clonal tree into the stack
+
+        Parameters
+        ----------
+        tree : ClonalTree
+            the clonal tree to be added to the stack
+        
+        '''
+ 
         tree.set_key(self.counter)
         self.counter += 1
         self.stack.append(tree)
 
     def index_tree(self, index):
+        '''gets a clonal tree from the stack by its index 
+
+        Parameters
+        ----------
+        index : int
+            the index for the tree to be obtained
+        
+        Returns
+        -------
+        a clonal tree at the specified index or None if the index does not exist
+
+        '''
         if index < self.size():
             return self.stack[index]
         return None
 
     def pop_tree(self):
+        '''pops a clonal tree from the stack
+        
+        Returns
+        -------
+        a clonal tree at the left most position in the stack
+
+        '''
         return self.stack.popleft()
 
     def get_tree(self, key):
+        '''gets a clonal tree from the stack by its internal key
+        
+        Returns
+        -------
+        a clonal tree with the specified key
+
+        '''
         for n in self.stack:
             if key == n.key:
                 return n
 
     def has_trees(self):
+        '''checks if the current stack has any clonal trees
+        
+        Returns
+        -------
+        a boolean specifiying if the stack has clonal trees
+
+        '''        
         return len(self.stack) > 0
 
     def get_total_count(self):
+        '''obtains the value of the counter, which counts the total number of 
+        trees added to the stack
+        
+        Returns
+        -------
+        counter : int
+            the value of the stacks internal counter
+
+        '''         
         return self.counter
 
     def size(self):
+        '''obtains the current length of the stack. This differs from the internal
+        counter, which counts all added trees and doesn't decrement the counter
+        when trees are popped.
+       
+        
+        Returns
+        -------
+        the current length of the stack
+
+        '''         
+
         return len(self.stack)
 
     def get_all_trees(self):
+        ''' gets the internal stack of all clonal trees
+       
+        
+        Returns
+        -------
+        the internal stack
+
+        ''' 
         return self.stack
 
     def contains(self, object):
+        ''' returns a boolean indicating if the specified object is in the stack
+       
+        Parameters
+        ----------
+        object : ClonalTree
+            the clonal tree object to be checked if it is contained in the current stack
+
+
+        Returns
+        -------
+        a boolean indicating if the given object is in the stack
+
+        ''' 
         for node in self.stack:
             if node == object:
                 return True
         return False
 
     def find_best_tree(self, data):
+        ''' traverses the stack of clonal trees and identifies the clonal tree with maximum likelihood
+       
+        Parameters
+        ----------
+        data : Data
+            a Phertilizer data object for which the likelihood of each clonal tree should be computed
 
+
+        Returns
+        -------
+        best_tree : ClonalTree
+            the clonal tree with maximum likelihood for the given data
+        
+        log_likelihood_series : pandas Series
+            a series with the tree keys as index and the loglikelihood of each tree as the data
+
+        ''' 
         n = self.size()
         print(f"Finding the maximum likelihood tree out of {n} trees....")
         log_likelihood_array = np.zeros(shape=n)
@@ -134,6 +233,15 @@ class ClonalTreeList:
         return best_tree, log_likelihood_series
 
     def save_the_trees(self, path):
+        ''' saves all the trees in the stack as pngs in the directory at the specified path
+       
+        Parameters
+        ----------
+        path : str
+            path there tree pngs should be saved
+
+        ''' 
+
         for tree in self.stack:
             loglike = np.round(tree.loglikelihood)
             if loglike >= 0:
@@ -145,6 +253,27 @@ class ClonalTreeList:
             tree.tree_png(fname)
 
     def find_elbow(self, epsilon=0.025):
+        ''' a regularizer that uses the elbow method to identify the output clonal tree
+       
+        Parameters
+        ----------
+        epsilon : float
+            the value to use for adding pseudolikelihoods for the max number of nodes + 1
+
+
+        Returns
+        -------
+        regularized_tree : ClonalTree
+            the clonal tree selected via the elbow method
+        
+        elbow_df : pandas Dataframe
+            a dataframe containing the pertinent information used to determine the elbow
+        
+        final_num_nodes : int
+            the number of nodes in the regularized tree
+
+        ''' 
+
         data = []
         max_node = 0
         for ct in self.stack:
